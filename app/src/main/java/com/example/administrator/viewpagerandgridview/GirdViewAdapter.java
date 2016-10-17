bb7b30fc5cc6dc3431bc1e4bef02492e92394d5b
Mr Su <suzhaoyong6120@gmail.com>
2016-10-17 12:56:01 +0800

    2016.10.17 真机未能连接

--- app/src/main/java/com/example/administrator/viewpagerandgridview/GirdViewAdapter.java
+++ app/src/main/java/com/example/administrator/viewpagerandgridview/GirdViewAdapter.java
@@ -1,8 +1,69 @@
 package com.example.administrator.viewpagerandgridview;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
 /**
  * Created by Administrator on 2016/10/14 0014.
- */
+*/
 
-public class GirdViewAdapter {
+public class GirdViewAdapter extends BaseAdapter{
+    private List<Model> mDates;
+    private LayoutInflater inflater;
+    private int curIndex;
+    private int pageSize;
+
+    public GirdViewAdapter(Context context, List<Model> mDates, int curIndex, int pageSize) {
+       inflater = LayoutInflater.from(context);
+        this.mDates = mDates;
+        this.curIndex = curIndex;
+        this.pageSize = pageSize;
+    }
+
+    @Override
+    public int getCount() {
+        return mDates.size() > (curIndex +1) * pageSize ? pageSize :
+                (mDates.size() - curIndex * pageSize);
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mDates.get(position + curIndex * pageSize);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position + curIndex * pageSize;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+       ViewHolder viewHolder = null;
+        if (viewHolder == null) {
+            convertView = inflater.inflate(R.layout.ite_gridview, parent, false);
+            viewHolder = new ViewHolder();
+            viewHolder.tv = (TextView) convertView.findViewById(R.id.textView);
+            viewHolder.iv = (ImageView) convertView.findViewById(R.id.imageView);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+        int pos = position + curIndex * pageSize ;
+        viewHolder.tv.setText(mDates.get(pos).name);
+        viewHolder.iv.setImageResource(mDates.get(pos).iconRes);
+
+        return null;
+    }
+
+    class ViewHolder {
+        public TextView tv;
+        public ImageView iv;
+    }
 }
--- app/src/main/java/com/example/administrator/viewpagerandgridview/MainActivity.java
+++ app/src/main/java/com/example/administrator/viewpagerandgridview/MainActivity.java
@@ -1,13 +1,91 @@
 package com.example.administrator.viewpagerandgridview;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    private String[] titles = {"美食", "电影", "酒店住宿", "休闲娱乐", "外卖", "自助餐", "KTV", "机票/火车票", "周边游", "美甲美睫",
+            "火锅", "生日蛋糕", "甜品饮品", "水上乐园", "汽车服务", "美发", "丽人", "景点", "足疗按摩", "运动健身", "健身", "超市", "买菜",
+            "今日新单", "小吃快餐", "面膜", "洗浴/汗蒸", "母婴亲子", "生活服务", "婚纱摄影", "学习培训", "家装", "结婚", "全部分配"};
+    private ViewPager mPager;
+    private List<View> mPagerList;
+    private List<Model> mDatas;
+    private LinearLayout mLlDot;
+    private LayoutInflater inflater;
+    private int pageCount;
+    private int pageSize = 10;
+    private int curIndex = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mPager = (ViewPager) findViewById(R.id.viewpager);
+        mLlDot = (LinearLayout) findViewById(R.id.ll_dot);
+        initDatas();
+        inflater = LayoutInflater.from(this);
+        pageCount = (int) Math.ceil(mDatas.size() * 1.0/ pageSize);
+        mPagerList = new ArrayList<View>();
+        for (int i = 0; i < pageCount; i++){
+            GridView gridView = (GridView) inflater.inflate(R.layout.gridview, mPager, false);
+            gridView.setAdapter(new GirdViewAdapter(this, mDatas, i, pageSize));
+            mPagerList.add(gridView);
+            gridView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                int pos = position + curIndex * pageSize;
+                    Toast.makeText(MainActivity.this, mDatas.get(pos).getName(), Toast.LENGTH_SHORT).show();
+                    
+                }
+            });
+        }
+        mPager.setAdapter(new ViewPageAdapter(mPagerList));
+        setOvalLayout();
+    }
+
+
+
+    private  void initDatas() {
+        mDatas = new ArrayList<Model>();
+        for (int i = 0; i < titles.length; i++){
+            int imageId = getResources().getIdentifier("ic_category_" + i,"mipmap", getPackageName());
+            mDatas.add(new Model(titles[i], imageId));
+        }
+    }
+
+    public void setOvalLayout() {
+        for (int i = 0; i < pageCount; i++){
+            mLlDot.addView(inflater.inflate(R.layout.dot, null));
+
+        }
+        mLlDot.getChildAt(0).findViewById(R.id.v_dot).setBackgroundResource(R.drawable.dot_selected);
+        mPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                mLlDot.getChildAt(curIndex).findViewById(R.id.v_dot).setBackgroundResource(R.drawable.dot_normal);
+                mLlDot.getChildAt(position).findViewById(R.id.v_dot).setBackgroundResource(R.drawable.dot_selected);
+                curIndex = position;
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
     }
 }
